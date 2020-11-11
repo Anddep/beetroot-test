d30a8f93e1f087555a8ebfbc10e6defc5cf5b438
Andrii Deputovych <andriid@ukieweb.com>
2020-11-12 00:28:02 +0200

    add responsive style for header, search form,  footer and download btn block

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -1,9 +1,32 @@
-$( ".header-menu ul li a" ).hover(
-    function() {
-        $( this ).addClass( "hover" );
-        $( '.header-menu' ).addClass( "hover" );
-    }, function() {
-        $( this ).removeClass( "hover" );
-        $( '.header-menu' ).removeClass( "hover" );
-    }
-);
+
+$( document ).ready(function() {
+
+    //hover menu
+    $( ".header-menu ul li a" ).hover(
+        function() {
+            $( this ).addClass( "hover" );
+            $( '.header-menu' ).addClass( "hover" );
+        }, function() {
+            $( this ).removeClass( "hover" );
+            $( '.header-menu' ).removeClass( "hover" );
+        }
+    );
+    //mobile menu
+    $( ".open-menu" ).click(function() {
+        $('.header-menu').addClass('show');
+        $('body').addClass('no-scroll');
+    });
+    $( ".close-menu" ).click(function() {
+        $('.header-menu').removeClass('show');
+        $('body').removeClass('no-scroll');
+    });
+
+    //change view list
+    $( ".sort-view-btn" ).click(function() {
+        let typeVIew = $( this ).attr('id');
+        $('.hotel-list').removeClass().addClass('hotel-list ' + typeVIew);
+        $( ".sort-view-btn" ).removeClass('active');
+        $(this).addClass('active');
+    });
+
+});
